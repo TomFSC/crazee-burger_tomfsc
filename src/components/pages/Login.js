d96809e002a478ad1224871f272065f4346d0264
Thomas FAISCA <faisca.thomas@gmail.com>
2022-10-04 22:19:32 +0200

    💄  F01: LoginPage> Add Render without CSS

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,14 @@
-import React from 'react'
-
 function LoginPage() {
+    //Render
   return (
-    <div>LoginPage</div>
+    <>
+    <h1>Bienvenue chez nous</h1>
+    <h2>Connectez-vous</h2>
+    <form action="submit">
+        <input type="text" placeholder='Entrez votre prénom...' required/>
+        <button>Accédez à votre espace</button>
+    </form>
+    </>
   )
 }
 
