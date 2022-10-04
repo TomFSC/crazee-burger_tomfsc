e0892548a80917534164ba0604a132107284d654
Thomas FAISCA <faisca.thomas@gmail.com>
2022-10-04 22:36:52 +0200

    👔 F01: LoginPage > Add state and business logic

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,12 +1,28 @@
+import { useState } from "react"
+
 function LoginPage() {
+    //State
+    const [firstName, setFirstName] = useState("")
+
+    //Business logic
+const handleChange = (event) => {
+    event.preventDefault()
+    setFirstName(event.target.value)
+}
+
+const handleSubmit = () => {
+    alert(`Bonjour ${firstName}`)
+    setFirstName("")
+}
+
     //Render
   return (
     <>
     <h1>Bienvenue chez nous</h1>
     <h2>Connectez-vous</h2>
-    <form action="submit">
-        <input type="text" placeholder='Entrez votre prénom...' required/>
-        <button>Accédez à votre espace</button>
+    <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Entrez votre prénom...' required onChange={handleChange}/>
+        <button >Accédez à votre espace</button>
     </form>
     </>
   )
