3424e20bc0238ea3bb644eb45ad655ee2f5d6d15
Thomas FAISCA <faisca.thomas@gmail.com>
2022-10-13 09:26:05 +0200

    ✨ F02: OrderPage> Create OrderPage and add routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import LoginPage from './components/pages/login/LoginPage';
-
-
+import "./App.css"
+import LoginPage from "./components/pages/login/LoginPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import OrderPage from "./components/pages/order/OrderPage"
 
 function App() {
-  return <LoginPage/>
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/order" element={<OrderPage />} />
+         </Routes>
+      </BrowserRouter>
+   )
 }
 
-export default App;
+export default App
---
+++ src/components/pages/order/OrderPage.js
@@ -0,0 +1,9 @@
+export default function OrderPage() {
+   return (
+      <>
+         <h1>Bonjour "USERNAME"</h1>
+         <br />
+         <button>Déconnexion</button>
+      </>
+   )
+}
