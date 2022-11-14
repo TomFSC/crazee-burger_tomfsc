5f850cf5f41f550822abb81168a8c227c1652301
Thomas FAISCA <faisca.thomas@gmail.com>
2022-11-14 14:05:22 +0100

    💄 F03: LoginPage> Add Background image

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,6 +1,22 @@
+import styled from "styled-components"
 import LoginForm from "./LoginForm"
+import backgroundImg from "../../../images/burger-background.jpg"
 
 export default function LoginPage() {
    //Render
-   return <LoginForm />
+   return (
+      <LoginContainerStyled>
+         <LoginForm />
+      </LoginContainerStyled>
+   )
 }
+
+const LoginContainerStyled = styled.div`
+   height: 100vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   background: url(${backgroundImg}) rgba(0, 0, 0, 0.7);
+   background-size: cover;
+   background-blend-mode: darken;
+`
